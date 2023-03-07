3f2439394ea6bb458f2f6739ec36a85a8c79e246
ScR4PP <95632351+JarcauCristian@users.noreply.github.com>
2023-03-07 15:39:20 +0000

    Done Mini Site

--- Laborator/app.js
+++ Laborator/app.js
@@ -1,6 +1,12 @@
-const btn = document.querySelector('.start_btn');
-const lnk = document.querySelector('.lnk');
+const btn = document.querySelector('.nav_btn');
+const one = document.querySelector('.one');
+const two = document.querySelector('.two');
+const three = document.querySelector('.three');
+const min_lnk = document.querySelector('.min_lnk');
 
 btn.addEventListener('click', function(){
-    lnk.classList.toggle('active');
+    one.classList.toggle('is_active');
+    two.classList.toggle('is_active');
+    three.classList.toggle('is_active');
+    min_lnk.classList.toggle('active');
 });

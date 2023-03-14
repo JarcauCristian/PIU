2323e6926387d70d5ddfc85594bb0b9538feedf0
ScR4PP <stefan.jarcau@student.unitbv.ro>
2023-03-14 15:35:16 +0200

    Added Site Angular

---
+++ Site Angular/Angular_Site/src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './body/home/home.component';
+import { InventoryComponent } from './body/inventory/inventory.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'inventory', component: InventoryComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Site Angular/Angular_Site/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './body/home/home.component';
+import { InventoryComponent } from './body/inventory/inventory.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    InventoryComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
